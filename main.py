5ada14d391d24ef2f1458650f97e2ec64afd8362
Johan Martin <0johan.martin0@gmail.com>
2022-02-03 08:33:58 +0100

    Config

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
 from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+
+class Bot():
+    def __init__(self):
+        # read configuration from json file
+        with open('config.json', 'r') as file:
+            config = json.load(file)
+
+        self.api_key = config['api_key']
+        self.api_secret = config['api_secret']
+        self.api_passphrase = config['api_passphrase']
+        self.sandbox = config['is_sandbox']
+        self.symbol_a = config['symbol_a']
+        self.symbol_b = config['symbol_b']
+        self.spread_mean = float(config['spread_mean'])
+        self.leverage = float(config['leverage'])
+        self.size = int(config['size'])
+        self.num_param = float(config['num_param'])
+        self.trade = Trade(self.api_key, self.api_secret, self.api_passphrase, is_sandbox=self.sandbox)
+        self.market = Market(self.api_key, self.api_secret, self.api_passphrase, is_sandbox=self.sandbox)
+
+
+if __name__ == '__main__':
+    hf = Bot()
+
+
+
+
+
+
+
+
+
+
+
 client = Client('', '')
 
 # get market depth
